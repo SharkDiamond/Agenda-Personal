22c2c191395877a76688962cd3cf9e298ebc1878
Gabriellaptop <gabrielarispe24@gmail.com>
2022-01-11 23:07:56 -0500

    Se le agrego una animacion al menu de noticias y se completo para que cuando sea click en una de las categorias traiga la informacion correspondiente

--- src/ColumnasComponents/ColumnaLeft/Noticias.js
+++ src/ColumnasComponents/ColumnaLeft/Noticias.js
@@ -3,15 +3,15 @@ import { useEffect,useState,useRef } from 'react';
 import axios from "axios";
 import {Card,Button} from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Container,Nav,Link  } from 'react-bootstrap';
+import './CL.css'
 
-import './ColumnaLeft.css';
 export default function Noticias() {
 
     //ESTADOS
     const [datosNoticias,setdatosNoticias]=useState([]);
     const [category,setCategory]=useState('Nacionales');
+    //REFERENCIAS
     const Titulo=useRef();
     const Menu=useRef();
 
@@ -32,9 +32,24 @@ export default function Noticias() {
     //CUANDO SE ACTUALICE EL ESTADO DE CATEGORIA
     useEffect(async()=>{
 
-      const Peticion= await axios.get(`https://newsapi.org/v2/top-headlines?&apiKey=cb2f0131b47344ebbc4af40883070deb`);
+      try {
 
-      setdatosNoticias(Peticion.data);
+      if (category!=='Nacionales') {
+        //REALIZANDO LA PETICION
+        const Peticion= await axios.get(`https://newsapi.org/v2/top-headlines/?apiKey=cb2f0131b47344ebbc4af40883070deb&sources=${category}`);
+        //ACTUALIZANDO EL ESTADO
+        setdatosNoticias(Peticion.data.articles);
+        //HACIENDO EL RETURN PARA QUE NO SE SIGA EJECUTANDO LA FUNCION
+        return;
+      }
+       //REALIZANDO LA PETICION
+       const Peticion= await axios.get(`https://newsapi.org/v2/top-headlines?country=co&apiKey=cb2f0131b47344ebbc4af40883070deb`);
+       //ACTUALIZANDO EL ESTADO
+       setdatosNoticias(Peticion.data.articles);
+
+      } catch (error) {
+          alert(error.message);
+      }
 
     },[category]);
 
@@ -59,29 +74,27 @@ export default function Noticias() {
     
    }
 
-
-
     return (
         <div  align="center">
-              <h1 className='text-white' ref={Titulo} onMouseEnter={()=>handleChangeTitle('Menu')}>NOTICIAS</h1>
+              <h1 className='text-white ' ref={Titulo} onMouseEnter={()=>handleChangeTitle('Menu')}>NOTICIAS</h1>
 
      <div className='d-none' ref={Menu} onMouseOut={()=>handleChangeTitle('Titulo')}>     
-<Navbar   >
-    <Container   >
-    <Nav className="me-auto" onMouseEnter={()=>handleChangeTitle('Menu')}>
+<Navbar className='animado'>
+    <Container >
+    <Nav className="me-auto" >
     <Nav.Link onClick={e=>setCategory('Nacionales')}  onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}}  href="#home" >Nacionales</Nav.Link>
-      <Nav.Link onClick={e=>setCategory('Economia')}  onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#home">Economia</Nav.Link>
-      <Nav.Link onClick={e=>setCategory('Tecnologia')} onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#features">Tecnologia</Nav.Link>
-      <Nav.Link onClick={e=>setCategory('Deportes')} onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#pricing">Deportes</Nav.Link>
+      <Nav.Link onClick={e=>setCategory('Fortune')}  onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#home">Economia</Nav.Link>
+      <Nav.Link onClick={e=>setCategory('ars-technica')} onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#features">Tecnologia</Nav.Link>
+      <Nav.Link onClick={e=>setCategory('bbc-sport')} onMouseEnter={()=>handleChangeTitle('Menu')} style={{ color:"blanchedalmond", "font-weight":"bold"}} href="#pricing">Deportes</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
-  </div>   
+     </div>   
 
             <div className='scroll'>
            {
     datosNoticias.map(element => 
-        <Card style={{ width: '100%'}} className='mb-2 prueba'>
+        <Card style={{ width: '100%'}} className='mb-2 prueba '>
         <Card.Img variant="top" src={element.urlToImage} />
         <Card.Body>
           <Card.Title>{element.title}</Card.Title>
